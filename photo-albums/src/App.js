bbd8d5479f54a8cd9e96233353d4134ca4ba21a5
MariaLuize <m.luize.s.pinheiro@gmail.com>
2020-10-09 14:37:29 -0300

    first use of GraphQL and queries

--- photo-albums/src/App.js
+++ photo-albums/src/App.js
@@ -1,39 +1,24 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React now
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 
-// export default App;
-
-import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 import { Header } from 'semantic-ui-react';
-class App extends Component { 
-  render() { 
-    return (
-      <div>
-        <Header as='h1'>Hello World!</Header>
-      </div>
-    );
-  }
+
+Amplify.configure(aws_exports);
+
+function App() {
+  return (
+    <Header as="h1">
+      Hello World!
+    </Header>
+  );
 }
-export default App;
+
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig: {
+    hiddenDefaults: ['phone_number']
+  }
+});
